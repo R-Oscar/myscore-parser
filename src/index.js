413e3e2fbf4970e5e92f847f5122198e01fefa99
R-Oscar <askar.payment@gmail.com>
2018-12-24 01:41:43 +0500

    working on hmr

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render as r } from 'react-dom';
+import App from './App';
+
+r(<App />, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  // devServer: {
+  //   // watchContentBase: true,
+  //   // publicPath: '/dist/',
+  //   hot: true
+  // },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+  module: {
+    rules: [{ test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
