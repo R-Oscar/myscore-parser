9376137ca0af1347517254be61ae7edcd45a7160
R-Oscar <askar.payment@gmail.com>
2018-12-24 14:59:04 +0500

    basic tests

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Select from '../Select';
 
 export default function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <p>ok</p>
+      <p>ok!</p>
+      <Select />
+      <Select />
     </div>
   );
 }
--- src/App/index.spec.js
+++ src/App/index.spec.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import App from './index';
+import Select from '../Select';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  it('calls componentDidMount', () => {
-    const wrapper = mount(<App />);
-    expect(App.prototype.componentDidMount).to.have.property('callCount', 1);
+  const wrapper = shallow(<App />);
+
+  it('renders the title', () => {
+    expect(wrapper.find('h1')).to.have.lengthOf(1);
+  });
+
+  it('renders the p tags', () => {
+    expect(wrapper.find('p')).to.have.lengthOf(2);
+  });
+
+  it('renders Select components', () => {
+    expect(wrapper.find(Select)).to.have.lengthOf(2);
   });
 });
