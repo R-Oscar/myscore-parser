d9505cf88d7a82aaf3c886e01db82a812afada2c
R-Oscar <askar.payment@gmail.com>
2018-12-24 19:51:15 +0500

    add material-ui

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import Results from '../Results';
 import Select from '../Select';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>ok</p>
-      <p>ok!</p>
+    <>
+      <CssBaseline />
+      <Typography variant="h4" gutterBottom>
+        Сбор статистики
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        Выберите страну
+      </Typography>
       <Select />
+      <Typography variant="subtitle1" gutterBottom>
+        Выберите лигу
+      </Typography>
       <Select />
-    </div>
+      <Results />
+    </>
   );
 }
--- src/App/index.spec.js
+++ src/App/index.spec.js
@@ -2,23 +2,31 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, configure } from 'enzyme';
 import App from './index';
+import Results from '../Results';
 import Select from '../Select';
 import Adapter from 'enzyme-adapter-react-16';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 
 configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   const wrapper = shallow(<App />);
 
-  it('renders the title', () => {
-    expect(wrapper.find('h1')).to.have.lengthOf(1);
+  it('renders the p tags!', () => {
+    expect(wrapper.find(Typography)).to.have.lengthOf(3);
   });
 
-  it('renders the p tags', () => {
-    expect(wrapper.find('p')).to.have.lengthOf(2);
-  });
-
-  it('renders Select components', () => {
+  it('renders Select components!', () => {
     expect(wrapper.find(Select)).to.have.lengthOf(2);
+  });
+
+  it('should render Results compo', () => {
+    expect(wrapper.find(Results)).to.have.lengthOf(1);
+  });
+
+  it('should include CssBaseline', () => {
+    expect(wrapper.find(CssBaseline)).to.have.lengthOf(1);
   });
 });
